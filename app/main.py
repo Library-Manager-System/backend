b409d254e03c3cd07de68948943c1848bb1aece8
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-10-20 11:45:47 -0300

    [ADD] CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import user
 from routers import admin
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(user.router)
 app.include_router(admin.router)

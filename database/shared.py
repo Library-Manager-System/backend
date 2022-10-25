5244c941da1a084d189aca8a49c2250375bfa1e4
Pogeres <94464569+Pogeres@users.noreply.github.com>
2022-10-25 15:03:56 -0300

    Add files via upload

---
+++ database/shared.py
@@ -0,0 +1,9 @@
+from database import Database
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = Database(user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'), host=os.getenv('DB_HOST'), database=os.getenv('DB_NAME'))
+
+

8dd08d5ab95ddc112e51476000e65250a15a2c47
EricLoBin <ekilobin@gmail.com>
2022-11-20 10:25:25 -0300

    [UPDATE] return user id

--- app/database/user.py
+++ app/database/user.py
@@ -4,7 +4,8 @@ import mysql.connector
 
 
 class User:
-    def __init__(self, name, email, password_hash, phone_number, address, user_type, confirmed_account = False) -> None:
+    def __init__(self, id, name, email, password_hash, phone_number, address, user_type, confirmed_account = False) -> None:
+        self.id = id
         self.name = name
         self.email = email
         self.__password_hash = password_hash
@@ -32,7 +33,7 @@ class User:
     def find(cls, email):
         query='SELECT * FROM tb_user WHERE email_user = %s'
         user = db.execute(query, [email])[0]
-        return User(user.name_user, user.email_user, user.password_user, user.phone_number_user, user.address_user, user.id_user_type, user.confirmed_account)
+        return User(user.id_user, user.name_user, user.email_user, user.password_user, user.phone_number_user, user.address_user, user.id_user_type, user.confirmed_account)
 
     def verify_password(self, password: str):
         return bcrypt.checkpw(password.encode(), self.__password_hash.encode())
